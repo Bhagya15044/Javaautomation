b7a711d8344df40bfd624c3c5d02a151c61d215b
Bhagya <bhagya27112000@gmail.com>
2025-05-14 13:21:53 +0530

    11 May 2025 - printing a 5th table using printf statement , without using loops

---
+++ src/com/project/task/com/project/task/Lab005_common_math_exception.java
@@ -0,0 +1,9 @@
+package com.project.task;
+
+public class Lab005_common_math_exception
+{
+    public static void main(String[] args)
+    {
+        System.out.println(  10/0 ); // if anything divide by zero we will get Arithmetic exception
+    }
+}
