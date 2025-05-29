c10261c9768b152d5a2e04ad642f0f2987d87021
Bhagya <bhagya27112000@gmail.com>
2025-05-29 17:31:34 +0530

    25th may - coding challenges(Armstrongnumber)

---
+++ src/com/project/task/com/project/coding_challenge_tasks/Lab1_Armstrong_Number.java
@@ -0,0 +1,36 @@
+package com.project.coding_challenge_tasks;
+
+import java.util.Scanner;
+
+public class Lab1_Armstrong_Number
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you like!!");
+        int number= scanner.nextInt();
+
+        int originalNumber = number;
+        int digits = String.valueOf(number).length();
+        int sum = 0;
+        if(number<10)
+        {
+            System.out.println("Invalid input!, please enter minimum two digit number ");
+        }
+
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += Math.pow(digit, digits);
+                number /= 10;
+            }
+
+        if (sum == originalNumber)
+        {
+            System.out.println(originalNumber + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNumber + " is not an Armstrong number.");
+        }
+
+    }
+}
